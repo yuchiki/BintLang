31dac8f08270849836d4f0a9d62ac7b5b539d367
yuchiki <14884013+yuchiki@users.noreply.github.com>
2025-11-10 21:54:03 +0900

    tokenizer unit testing

--- BintLangLib.Tests/Tests.fs
+++
@@ -1,8 +0,0 @@
-﻿module Tests
-
-open Xunit
-
-[<Fact>]
-let ``Ensure BintLangLib.Parse is available`` () =
-    let _ = Parser.Parse
-    Assert.True(true)
---
+++ BintLangLib.Tests/Tokenizer.Tests.fs
@@ -0,0 +1,46 @@
+﻿module TokenizerTests
+
+open Xunit
+open Tokens
+
+
+let mustSucceed: Result<token list, exn> -> token list =
+    function
+    | Error err -> failwith $"unexpected error: {err}"
+    | Ok res -> res
+
+let mustFail: Result<token list, exn> -> unit =
+    function
+    | Ok res -> Assert.Fail $"Expected error but got: {res}"
+    | Error err -> Assert.Contains("$%", err.ToString())
+
+[<Fact>]
+let ``Tokenizer.matchString returns empty for empty string`` () =
+    let input: string = ""
+    let expected: token list = []
+
+    let res = mustSucceed (Tokenizer.matchString input)
+    Assert.Equal<token list>(expected, res)
+
+[<Fact>]
+let ``Tokenizer.matchString tokenizes simple symbols`` () =
+    let input: string = "@(),"
+    let expected: token list = [ Leaf; LParen; RParen; Comma ]
+
+    let res = mustSucceed (Tokenizer.matchString input)
+    Assert.Equal<token list>(expected, res)
+
+
+[<Fact>]
+let ``Tokenizer.matchString tokenizes ignoring spaces`` () =
+    let input: string = "@(  ),"
+    let expected: token list = [ Leaf; LParen; RParen; Comma ]
+
+    let res = mustSucceed (Tokenizer.matchString input)
+    Assert.Equal<token list>(expected, res)
+
+
+[<Fact>]
+let ``Tokenizer.matchString fails for invalid string`` () =
+    let input: string = "abc$%"
+    Tokenizer.matchString input |> mustFail
