1dca3ab10bfae206af9529ee555feaa8b7bb796f
yuchiki <14884013+yuchiki@users.noreply.github.com>
2025-11-10 22:22:46 +0900

    refactor tokenizertest

--- BintLangLib.Tests/Tokenizer.Tests.fs
+++ BintLangLib.Tests/Tokenizer.Tests.fs
@@ -3,44 +3,34 @@
 open Xunit
 open Tokens
 
-
-let mustSucceed: Result<token list, exn> -> token list =
+let mustSucceed: Result<'a, exn> -> 'a =
     function
     | Error err -> failwith $"unexpected error: {err}"
     | Ok res -> res
 
-let mustFail: Result<token list, exn> -> unit =
+let mustSucceedAs (expected: 'a) (res: Result<'a, exn>) : unit =
+    Assert.Equal<'a>(expected, res |> mustSucceed)
+
+let mustFail: Result<'a, exn> -> unit =
     function
     | Ok res -> Assert.Fail $"Expected error but got: {res}"
     | Error err -> Assert.Contains("$%", err.ToString())
 
-[<Fact>]
-let ``Tokenizer.matchString returns empty for empty string`` () =
-    let input: string = ""
-    let expected: token list = []
+let matchMustSucceedAs (expected: token list) (input: string) : unit =
+    Tokenizer.matchString input |> mustSucceedAs expected
 
-    let res = mustSucceed (Tokenizer.matchString input)
-    Assert.Equal<token list>(expected, res)
+let matchMustFail: string -> unit = Tokenizer.matchString >> mustFail
+
+[<Fact>]
+let ``Tokenizer.matchString returns empty for empty string`` () = matchMustSucceedAs [] ""
 
 [<Fact>]
 let ``Tokenizer.matchString tokenizes simple symbols`` () =
-    let input: string = "@(),"
-    let expected: token list = [ Leaf; LParen; RParen; Comma ]
-
-    let res = mustSucceed (Tokenizer.matchString input)
-    Assert.Equal<token list>(expected, res)
-
+    matchMustSucceedAs [ Leaf; LParen; RParen; Comma ] "@(),"
 
 [<Fact>]
 let ``Tokenizer.matchString tokenizes ignoring spaces`` () =
-    let input: string = "@(  ),"
-    let expected: token list = [ Leaf; LParen; RParen; Comma ]
-
-    let res = mustSucceed (Tokenizer.matchString input)
-    Assert.Equal<token list>(expected, res)
-
+    matchMustSucceedAs [ Leaf; LParen; RParen; Comma ] "@(  ),"
 
 [<Fact>]
-let ``Tokenizer.matchString fails for invalid string`` () =
-    let input: string = "abc$%"
-    Tokenizer.matchString input |> mustFail
+let ``Tokenizer.matchString fails for invalid string`` () = matchMustFail "abc$%"
